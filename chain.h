f28c050a7bae81ed99d3adf17f119f43f6de45d8
Addeline Mitchell <amitchell@alaska.edu>
2016-05-01 00:19:06 -0800

    Added comments.

--- chain.h
+++ chain.h
@@ -1,6 +1,6 @@
 // chain.h
 // Addeline Mitchell
-// 29 Apr 2016
+// 1 May 2016
 //
 // For CS 372 Spring 2016, Assn 4 Exercise A
 // Header file for chain class
@@ -9,23 +9,34 @@
 #ifndef CHAIN_H
 #define CHAIN_H
 
+
 #include "nameGen.h"
 
+
+// Chain
+// this class is used to create & move through responsibility
+// chains made up of objects from itself & its derived classes
 class Chain : public Name
 {
 protected:
 	Chain *next;
 
 public:
+	// default ctor
 	Chain()
 	{ next = nullptr; }
 
-	void nxt(Chain *n)
+	// link
+	// links objects from Chain & its derived classes
+	void link(Chain *n)
 	{
-		if (next) { next->nxt(n); }
+		if (next) { next->link(n); }
 		else { next = n; }
 	}
 
+	// handle
+	// takes string through chain so it can be handled
+	// overidden in derived classes
 	virtual void handle(string str) // go to next in chain
 	{ next->handle(str); }
 };
